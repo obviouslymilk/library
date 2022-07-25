bdc8edc2de658808025385a27b6884bf9ca806cc
Klim Storozhenko <astrogue.games@gmail.com>
2022-07-25 11:12:50 +1000

    Add form

--- script.js
+++ script.js
@@ -1,30 +1,60 @@
 let myLibrary = [];
 
+const newButton = document.querySelector('.new-button');
+const booksContainer = document.querySelector('.books-container');
+const bookAddForm = document.querySelector('.form-container');
+const bookFormSubmit = document.querySelector('.submit');
+const bookFormClose = document.querySelector('.close');
 
 function Book(title, author, pages, read) {
         this.title = title;
         this.author = author;
         this.pages = pages;
         this.read = read;
+        this.index = myLibrary.length
 
-        this.info = () => {
-            return title + " by " + author + ", " + pages + " pages, " + (read ? "already read" : "nor read yet") + ".";
-        }
+        myLibrary.push(this)
 }
 
+Book.prototype.info = function() {
+    return this.title + " by " + this.author + ", " + this.pages + " pages, " + (this.read ? "already read" : "nor read yet") + ".";
+}
+
+function BookCard(book) {
+    return `
+    <div class='book-card' data-pos=${book.index}>
+        <div class='book-title'>«${book.title}»</div>
+        <div class='book-author'>by ${book.author}</div>
+        <div class='book-pages'>${book.pages} pages</div>
+    </div>
+    `
+}
 
 function addBookToLibrary(book) {
     myLibrary.push(book)
 }
 
 function fetchLibrary() {
+    booksContainer.replaceChildren('');
+
     myLibrary.forEach(
-        e => console.log(e)
-    )
+        e => booksContainer.insertAdjacentHTML('beforeend', BookCard(e))
+    );
 }
 
+function submitForm(e) {
+    data = new FormData(bookAddForm);
+    title = data.get('title');
+    author = data.get('author')
+    pages = data.get('pages');
 
-theHobbit = new Book('The Hobbit', 'J.R.R. Tolkien', 295, false);
-addBookToLibrary(theHobbit)
 
-fetchLibrary()
+    book = new Book(title, author, pages, false);
+    
+    fetchLibrary();
+    e.preventDefault();
+}
+
+fetchLibrary();
+
+bookAddForm.addEventListener('submit', submitForm)
