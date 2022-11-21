6e4be2a8c1d79f61307abee8ca6153a5aa4fbe4e
KLIM STOROZHENKO <storozhenko.klim@gmail.com>
2022-11-21 13:47:26 +1000

    Refactor library logic

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 //! ПОФИКСТИЬ УДАЛЕНИЕ
-let myLibrary = [];
+let myLibrary = {};
 
 const newButton = document.querySelector('.new-button');
 const booksContainer = document.querySelector('.books-container');
@@ -14,71 +14,60 @@ class Book {
         this.author = author;
         this.pages = pages;
         this.read = read;
-        this.index = myLibrary.length;
-
-        myLibrary.push(this);
-    }
-
-    info() {
-        return this.title + " by " + this.author + ", " + this.pages + " pages, " + (this.read ? "already read" : "nor read yet") + ".";
     }
 }
 
-
-function BookCard(book) {
-    var bookCard = document.createElement('div');
-    bookCard.classList.add('book-card');
-    bookCard.dataset.pos = book.index;
-
-    bookCard.insertAdjacentHTML('beforeend', 
-        `<div class='book-title'>«${book.title}»</div>
-        <div class='book-author'>by ${book.author}</div>
-        <div class='book-pages'>${book.pages} pages</div>`
-    )
-
-    var readButton = document.createElement('button');
-    readButton.classList.add('read');
-    console.log(book.read);
-    if (book.read === true) {
-        readButton.classList.add('on');
-    }
-    readButton.addEventListener('click', e => {
-        e.target.classList.toggle('on')
-        book.read = true
-    })
-
-    var removeButton = document.createElement('button');
-    removeButton.classList.add('remove');
-    removeButton.addEventListener('click', e => {
-        e.target.parentElement.remove();
-    });
-    removeButton.innerText = 'Remove';
-
-    bookCard.insertAdjacentElement('beforeend', readButton);
-    bookCard.insertAdjacentElement('beforeend', removeButton);
-
-    return bookCard;
-}
+const createBookElement = book => `
+    <div class="book">
+        <h1 class="book-title">«${book.title}»</h1>
+        <h2 class="book-author">by ${book.author}</h2>
+        <h3 class="book-pages">${book.pages} pages</h3>
+        <div class="buttons-container">
+            <button class="read ${book.read ? "on" : null}" data-title=${book.title}>${book.read ? "Already read" : "Not read yet"}</button>
+            <button data-title=${book.title} class="remove">Remove</button>
+        </div>
+    </div>
+`
 
 function fetchLibrary() {
     booksContainer.replaceChildren('');
+    
+    Object.entries(myLibrary).forEach(entry => {
+        booksContainer.insertAdjacentHTML('beforeend', createBookElement(entry[1]));
+    })
+}
 
-    myLibrary.forEach(
-        e => booksContainer.insertAdjacentElement('beforeend', BookCard(e))
-    );
+function addBook(title, author, pages, read = false) {
+    if (myLibrary[title])
+        return alert('This book is already in library.');
+    
+    const book = new Book(title, author, pages, read);
+    
+    myLibrary[title] = book;
+}
+
+function removeBook(e) {
+    const title = e.target.dataset.title;
+    delete myLibrary[title];
+    fetchLibrary();
+}
+
+function changeReadStatus(e) {
+    const title = e.target.dataset.title;
+    myLibrary[title].read = !myLibrary[title].read;
+    fetchLibrary();
 }
 
 function submitForm(e) {
+    e.preventDefault();
+
     data = new FormData(bookAddForm);
     title = data.get('title');
     author = data.get('author')
     pages = data.get('pages');
-
-
-    new Book(title, author, pages, false);
     
+    addBook(title, author, pages);
     fetchLibrary();
-    e.preventDefault();
 
     bookAddForm.reset();
     formPopup.style.display = 'none'
@@ -86,8 +75,16 @@ function submitForm(e) {
 
 
 fetchLibrary();
+bookAddForm.addEventListener('submit', submitForm);
+newButton.addEventListener('click', () => formPopup.style.display = 'flex')
+bookFormClose.addEventListener('click', () => formPopup.style.display = 'none')
 
-bookAddForm.addEventListener('submit', submitForm)
+document.querySelector('body').addEventListener('click', function(e) {
+    if ( e.target.classList.contains('remove') ) {
+        removeBook(e);
+    }
+    if ( e.target.classList.contains('read') ) {
+        changeReadStatus(e);
+    }
 
-newButton.addEventListener('click', e => formPopup.style.display = 'flex')
-bookFormClose.addEventListener('click', e => formPopup.style.display = 'none')
+}, true);
